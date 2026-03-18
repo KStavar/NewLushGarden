5fc54ecba75ede49ab628644c0873f3258e357fc
Kate <katelenovoa396@gmail.com>
2026-03-18 11:00:54 +0200

    push

--- src/components/lib/ModalWindow.jsx
+++ src/components/lib/ModalWindow.jsx
@@ -1,32 +1,17 @@
 import styles from '@/style/ModalWindow.module.css'
 
-export default function Modal({ onClose }) {
+export default function ModalWindow({ onClose, title, children }) {
   return (
     <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         <div className={styles.popupContent}>
           <button className={styles.modalClose} onClick={onClose}>×</button>
-          <h2>
-            Enter Your Email Address For Our <br/>
-            Mailing Promo or Other Interesting <br/>
-            Things
-          </h2>
-
+          {title && <h2>{title}</h2>}
           <div className={styles.emailButton}>
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className={styles.inputEmail}
-            />
-
-            <button className={styles.submitButton}>
-              <a className={styles.navLink} href="" target="_blank">
-                <p>Submit</p>
-              </a>
-            </button>
+            {children}
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,10 +1,11 @@
 'use client'
 import Image from "next/image"
 import Link from "next/link"
-import styles from '@/style/Header.module.css'
+import modalStyles from '@/style/ModalWindow.module.css'
+import headerStyles from '@/style/Header.module.css'
 import { usePathname } from "next/navigation"
 import { useState } from "react"
-import Modal from "../lib/ModalWindow"
+import ModalWindow from "../lib/ModalWindow"
 import Contact from "./Contact"
 
 export default function Header() {
@@ -13,31 +14,48 @@ export default function Header() {
   const [openContact, setOpenContact] = useState(false);
 
   return (
-    <header className={pathname === '/' ? styles.header : styles.headerGradient}>
-      <div className={styles.logo}>
+    <header className={pathname === '/' ? headerStyles.header : headerStyles.headerGradient}>
+      <div className={headerStyles.logo}>
         <Link href="/">
           <Image src="/Mask_group.png" width={100} height={60} alt="logo" />
         </Link>
       </div>
 
-      <nav className={styles.navContainer}>
-        <ul className={styles.navList}>
-          <li className={styles.navListItem}>
-            <Link href="/" className={styles.navLink}>Home</Link>
+      <nav className={headerStyles.navContainer}>
+        <ul className={headerStyles.navList}>
+          <li className={headerStyles.navListItem}>
+            <Link href="/" className={headerStyles.navLink}>Home</Link>
           </li>
-          <li className={styles.navListItem}>
-            <Link href="/about-us" className={styles.navLink}>About Us</Link>
+          <li className={headerStyles.navListItem}>
+            <Link href="/about-us" className={headerStyles.navLink}>About Us</Link>
           </li>
-          <li className={styles.navListItem}>
-            <Link href="/gallery" className={styles.navLink}>Gallery</Link>
+          <li className={headerStyles.navListItem}>
+            <Link href="/gallery" className={headerStyles.navLink}>Gallery</Link>
           </li>
-          <li className={styles.contactNavListItem} onClick={() => setOpenContact(!openContact)}>Contact</li>
-          <li className={styles.separatedNavListItem} onClick={() => setOpen(!open)}>Details</li>
+          <li className={headerStyles.contactNavListItem} onClick={() => setOpenContact(!openContact)}>Contact</li>
+          <li className={headerStyles.separatedNavListItem} onClick={() => setOpen(!open)}>Details</li>
         </ul>
       </nav>
 
       {openContact && <Contact onClose={() => setOpenContact(false)} />}
-      {open && <Modal onClose={() => setOpen(false)} />}
+
+      {open && (
+        <ModalWindow
+          onClose={() => setOpen(false)} 
+          title="Enter Your Email Address For Our Mailing Promo"
+        >
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className={`${modalStyles.inputEmail} ${headerStyles.inputEmailExtra}`}
+          />
+          <button className={`${modalStyles.submitButton} ${headerStyles.specialButton}`}>
+            <a className={`${modalStyles.navLink} ${headerStyles.navLinkExtra}`} href={window.location.pathname}>
+              <p>Submit</p>
+            </a>
+          </button>
+        </ModalWindow>
+      )}
     </header>
   )
 }
